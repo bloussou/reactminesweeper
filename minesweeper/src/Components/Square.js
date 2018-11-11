a61ae2f482df33f62341457a332110def07074fe
Brieuc Loussouarn <brieuc.loussouar@student.ecp.fr>
2018-11-11 11:08:37 +0100

    New grid size

--- minesweeper/src/Components/Square.js
+++ minesweeper/src/Components/Square.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Square(props) {
     return (
-        <button className="square" onClick={props.onClick} onContextMenu={props.onRightClick} style={{ fontSize: 5, width: 10, height: 10 }}>
+        <button className="square" onClick={props.onClick} onContextMenu={props.onRightClick} style={{ fontSize: 10, width: 30, height: 30 }}>
             {props.value}
         </button>
     );
